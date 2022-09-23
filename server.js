b57da051e24b519c19901a7b3905f3ff6683f0f9
Raskirat <rkohli21@myseneca.ca>
2022-09-23 18:16:14 -0400

    npm commit

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+var path = require("path");
+//var dataServ = require("data-service")
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
   }
 app.use(express.static('public')); 
 // setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
-});
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
-});
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+  });
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+  });
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
