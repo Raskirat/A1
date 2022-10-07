1ef924d41a9b177d14d98d379fdfa948ab052c97
Raskirat <rkohli21@myseneca.ca>
2022-10-07 18:13:48 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Raskirat Singh Kohli Student ID: 149660219 Date: 10/7/2022
+*
+* Your app’s URL (from Cyclic) : ______________________________________________
+*
+*************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require("path");
-//var dataServ = require("data-service")
+var dataServ = require("./data-service")
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
   }
@@ -14,5 +26,27 @@ app.get("/", function(req,res){
 app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname,"/views/about.html"));
   });
+app.get("/employees", function(req,res){
+  dataServ.getAllEmployees()
+  .then((data) => { res.json(data) })
+  .catch((err) => { res.json({message: err}) });
+});
+app.get("/managers", function(req,res){
+  dataServ.getManagers()
+  .then((data) => { res.json(data) })
+  .catch((err) => { res.json({message: err}) });
+});
+app.get("/departments", function(req,res){
+  dataServ.getDepartments()
+  .then((data) => { res.json(data) })
+  .catch((err) => { res.json({message: err}) });
+});
+app.get("*", function(req,res){
+  res.send("Uh Oh! Error 404: File Not Found");
+});
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+dataServ.initialize()
+.then(() => {app.listen(HTTP_PORT, onHttpStart)})
+.catch(function(reason){
+  console.log(reason);
+});
